e2dd4685ffab8623052fee87ccf80bf6d4bf8834
VA1357 <68082963+VA1357@users.noreply.github.com>
2026-03-30 20:38:51 -0400

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        #repeats create extra work so remove at start
+        nums = set(nums)
+        starts = []
+        #start each sequence with numbers that dont have any before them, then use set O(1) lookups and brute force
+        for i in nums:
+            if i-1 not in nums:
+                starts.append(i)
+        res = 0
+        #brute force, for each sequence count manually using O(1) lookups
+        for i in starts:
+            count = 1
+            val = i
+            while val+1 in nums:
+                val +=1
+                count+=1
+            res = max(res, count)
+        return res
+        #try to consolidate into one loop?
